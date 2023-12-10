b76fcbe524bda3261c9bead9c8ff55e734bb1539
Jessica Lee <jessicalee936@gmail.com>
2023-12-09 22:33:21 -0500

    Complete create/update/delete for Posts

--- src/main/java/com/example/tdd/demo/controller/PostController.java
+++ src/main/java/com/example/tdd/demo/controller/PostController.java
@@ -1,12 +1,11 @@
 package com.example.tdd.demo.controller;
 
+import com.example.tdd.demo.exception.PostNotFoundException;
 import com.example.tdd.demo.record.Post;
 import com.example.tdd.demo.repo.PostRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +27,43 @@ public class PostController {
 
     @GetMapping("/{id}")
     Optional<Post> findById(@PathVariable Integer id){
-        return postRepository.findById(id);
+        return Optional.of(postRepository.findById(id)).orElseThrow(PostNotFoundException::new);
     }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    Post create(@Valid @RequestBody Post post){
+        return postRepository.save(post);
+    }
+
+    @PutMapping("/{id}")
+    Post update(@PathVariable Integer id, @Valid @RequestBody Post post){
+        Optional<Post> existingPost = postRepository.findById(id);
+        if(existingPost.isPresent()){
+            Post updatePost = new Post(
+                    existingPost.get().id(),
+                    existingPost.get().userId(),
+                    post.title(),
+                    post.body(),
+                    existingPost.get().version()
+            );
+
+            return postRepository.save(updatePost);
+        }else{
+            throw new PostNotFoundException();
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id){
+        Optional<Post> post = postRepository.findById(id);
+
+        if(post.isPresent()){
+            postRepository.delete(post.get());
+        }else{
+            throw new PostNotFoundException();
+        }
+    }
+
 }
--- src/test/java/com/example/tdd/demo/PostControllerTest.java
+++ src/test/java/com/example/tdd/demo/PostControllerTest.java
@@ -1,11 +1,11 @@
 package com.example.tdd.demo;
 
 import com.example.tdd.demo.controller.PostController;
+import com.example.tdd.demo.exception.PostNotFoundException;
 import com.example.tdd.demo.record.Post;
 import com.example.tdd.demo.repo.PostRepository;
-//import org.junit.Test;  // incompatible when using junit and jupiter tgt
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,8 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -84,6 +84,122 @@ public class PostControllerTest {
         mockMvc.perform(get("/api/posts/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(jsonResponse));
+    }
+
+    @Test
+    public void shouldNotFindPost_whenGivenInvalidId() throws Exception {
+        when(postRepository.findById(999)).thenThrow(PostNotFoundException.class);
+
+        mockMvc.perform(get("api/posts/999"))
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    public void shouldCreateNewPost_whenGivenValidPost() throws Exception {
+        String postRequest = """
+                    {
+                        "id":3,
+                        "userId":3,
+                        "title":"Hello",
+                        "body":"test",
+                        "version": null
+                    }
+                """;
+
+        Post newPost = new Post(3,3, "Hello","test",null);
+        when(postRepository.save(newPost)).thenReturn(newPost);
+
+        mockMvc.perform(post("/api/posts")
+                        .contentType("application/json")
+                        .content(postRequest))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(postRequest));
+    }
+
+    @Test
+    public void shouldNotCreateNewPost_whenGivenInValidPost() throws Exception {
+        String postRequest = """
+                    {
+                        "id":3,
+                        "userId":3,
+                        "title":"",
+                        "body":"",
+                        "version": null
+                    }
+                """;
+
+        Post newPost = new Post(3,3, "","",null);
+        when(postRepository.save(newPost)).thenReturn(newPost);
+
+        mockMvc.perform(post("/api/posts")
+                        .contentType("application/json")
+                        .content(postRequest))
+                .andExpect(status().isBadRequest());
+    }
+
+
+
+    @Test
+    void shouldUpdatePost_whenGivenValidPost() throws Exception {
+        String postRequest = """
+                    {
+                        "id":1,
+                        "userId":1,
+                        "title":"New Title",
+                        "body":"New body message",
+                        "version": null
+                    }
+                """;
+
+        Post updatedPost = new Post(1,1, "New Title","New body message",null);
+        when(postRepository.findById(1)).thenReturn(Optional.of(posts.get(0)));
+        when(postRepository.save(updatedPost)).thenReturn(updatedPost);
+
+        mockMvc.perform(put("/api/posts/1")
+                        .contentType("application/json")
+                        .content(postRequest))
+                .andExpect(status().isOk())
+                .andExpect(content().json(postRequest));
+
+    }
+
+    @Test
+    void shouldNotUpdatePost_whenGivenInvalidPost() throws Exception {
+        String postRequest = """
+                    {
+                        "id":1,
+                        "userId":1,
+                        "title":"",
+                        "body":"",
+                        "version": null
+                    }
+                """;
+
+        mockMvc.perform(put("/api/posts/1")
+                        .contentType("application/json")
+                        .content(postRequest))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldDeletePost_whenGivenValidId() throws Exception {
+
+        when(postRepository.findById(1)).thenReturn(Optional.of(posts.get(0)));
+        doNothing().when(postRepository).delete(posts.get(0));
+        mockMvc.perform(delete("/api/posts/1"))
+                .andExpect(status().isNoContent());
+        verify(postRepository,times(1)).delete(posts.get(0));
+    }
+
+    @Test
+    void shouldNotDeletePost_whenGivenInvalidId() throws Exception {
+
+        when(postRepository.findById(999)).thenThrow(PostNotFoundException.class);
+
+        mockMvc.perform(delete("/api/posts/999"))
+                .andExpect(status().isNotFound());
+
     }
 
 }
